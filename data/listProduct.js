69632adbab33459c04a32633a5e3ad7a453a4d74
Nguyen Minh truc <nguyenminhtruc@192.168.2.43>
2023-11-01 16:09:03 +0700

    add data type

--- data/listProduct.js
+++ data/listProduct.js
@@ -4,70 +4,74 @@
         id : 1,
         url : "../assets/img/listProduct/product_1.webp",   
         name : "áo thun basic V details",
-        price: "10$"
+        price: "10$",
+        type: "áo"
 
     },
     {
         id : 2,
         url : "../assets/img/listProduct/product_2.webp",   
         name : "Áo Polo Wool Ziczac Waves",
-        price: "12$"
+        price: "12$",
+        type: "áo"
 
     },
     {
         id : 3,
         url : "../assets/img/listProduct/product_3.jpeg",   
         name : "Áo Polo Nerdy Brown Sleeves",
-        price: "12$"
+        price: "12$",
+        type: "áo"
 
     },
     {
         id : 4,
         url : "../assets/img/listProduct/product_4.webp",   
         name : "Quần Tây Single Linen Base",
-        price: "15$"
+        price: "15$",
+        type: "quần"
 
     },
     {
         id : 5,
         url : "../assets/img/listProduct/product_5.webp",   
         name : "Quần Dài Orange Label Slim",
-        price: "14$"
-
+        price: "14$",
+        type: "quần"
     },
     {
         id : 6,
         url : "../assets/img/listProduct/product_6.webp",   
         name : "Áo Thun Boxy Hugging Beige",
-        price: "10$"
-
+        price: "10$",
+        type: "áo"
     },
     {
         id : 7,
         url : "../assets/img/listProduct/product_7.webp",   
         name : "Quần Jean Skinny White Label Ironic",
-        price: "20$"
-
+        price: "20$",
+        type: "quần"
     },
     {
         id : 8,
         url : "../assets/img/listProduct/product_8.webp",   
         name : "Áo Thun Lolipop White 360GSM",
-        price: "13$"
-
+        price: "13$",
+        type: "áo"
     },
     {
         id : 9,
         url : "../assets/img/listProduct/product_9.webp",   
         name : "Giày Loafer Classic Frontape Creamy",
-        price: "25$"
-
+        price: "25$",
+        type: "giày"
     },
     {
         id : 10,
         url : "../assets/img/listProduct/product_10.webp",   
         name : "Ví Abstract Diamond Edge",
-        price: "30$"
-
+        price: "30$",
+        type: "ví"
     }
 ]
