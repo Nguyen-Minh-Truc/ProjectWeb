b4826d8c34a8feda91279f35466b3ecc515c27a1
Nguyen Minh truc <nguyenminhtruc@MacBook-Air-cua-Nguyen.local>
2023-11-03 16:04:29 +0700

    showProduct

--- home/assets/scrip.js
+++ home/assets/scrip.js
@@ -1,6 +1,7 @@
 import { listProductData } from "../../data/listProduct.js";
 
-const listProducts =JSON.parse(localStorage.getItem("listProducts")) || listProductData;
+const listProducts =
+  JSON.parse(localStorage.getItem("listProducts")) || listProductData;
 function createListProduct(listProducts) {
   const container = document.querySelector(".list__product__container");
   const product = listProducts.map((product) => {
@@ -46,7 +47,7 @@ itemMenu.forEach((item) => {
       }
       if (result) {
         createListProduct(listProductResult);
-        listProductResult = []
+        listProductResult = [];
       } else {
         createListProduct(listProducts);
       }
@@ -65,7 +66,7 @@ function chuyenChuoiInHoaKhongDau(chuoi) {
 const searchInput = document.getElementById("search__product");
 const searchResults = document.querySelector(".btn__search");
 
-searchInput.addEventListener("input", function() {
+searchInput.addEventListener("input", function () {
   const searchTerm = chuyenChuoiInHoaKhongDau(searchInput.value);
 
   const listProducts = JSON.parse(localStorage.getItem("listProducts"));
@@ -86,15 +87,16 @@ searchInput.addEventListener("input", function() {
     if (result) {
       createListProduct(listProductResult);
     } else {
-      createListProduct(listProducts); 
-      
+      createListProduct(listProducts);
     }
   }
 });
 
 // search Product
 function searchProduct() {
-  const keyWord = chuyenChuoiInHoaKhongDau(document.getElementById("search__product").value);
+  const keyWord = chuyenChuoiInHoaKhongDau(
+    document.getElementById("search__product").value
+  );
   const listProducts = JSON.parse(localStorage.getItem("listProducts"));
   const listProductResult = [];
 
@@ -124,3 +126,43 @@ function searchProduct() {
 searchResults.addEventListener("click", () => {
   searchProduct();
 });
+// showProduct
+
+const productInfo = document.querySelector("#productInfo");
+const callBlocks = document.querySelectorAll(".product__item");
+
+callBlocks.forEach((callBlock, i) => {
+  callBlock.addEventListener("click", () => {
+    productInfo.classList.add("active");
+    productInfo.classList.remove("unactive");
+    document.getElementById("imgbig").src = document.querySelectorAll(".product__img")[i].src;
+    document.getElementById("productname").innerHTML = document.querySelectorAll(".product__name")[i].textContent;
+    document.getElementById("productprice").innerHTML = document.querySelectorAll(".product__price")[i].textContent;
+    const closeBtn = document.querySelector(".close");
+    closeBtn.addEventListener("click", () => {
+      productInfo.classList.remove("active");
+      productInfo.classList.add("unactive");
+      document.querySelector(".quantity-input").value = 1;
+      document.getElementById("size").value = document.getElementById("size").options[0].value;
+    });
+  });
+});
+// Lấy các phần tử HTML cần thao tác
+const decreaseButton = document.querySelector(".decrease");
+const increaseButton = document.querySelector(".increase");
+const quantityInput = document.querySelector(".quantity-input");
+
+// Định nghĩa sự kiện khi nút "-" được nhấn
+decreaseButton.addEventListener("click", () => {
+  let currentValue = parseInt(quantityInput.value);
+  if (currentValue > 1) {
+    quantityInput.value = (currentValue - 1).toString();
+  }
+});
+
+// Định nghĩa sự kiện khi nút "+" được nhấn
+increaseButton.addEventListener("click", () => {
+  let currentValue = parseInt(quantityInput.value);
+  quantityInput.value = (currentValue + 1).toString();
+});
+
