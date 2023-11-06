a85a399ad3aef4898e3f60ccaacd9ca54da43ed1
Nguyen Minh truc <nguyenminhtruc@192.168.2.43>
2023-11-06 17:45:20 +0700

    showProduct

--- data/listProduct.js
+++ data/listProduct.js
@@ -5,7 +5,7 @@
         url : "../assets/img/listProduct/product_1.webp",   
         name : "áo thun basic V details",
         price: "10",
-        type: "áo"
+        type: "ao thun"
 
     },
     {
@@ -13,7 +13,7 @@
         url : "../assets/img/listProduct/product_2.webp",   
         name : "Áo Polo Wool Ziczac Waves",
         price: "12",
-        type: "áo"
+        type: "áo Polo"
 
     },
     {
@@ -21,7 +21,7 @@
         url : "../assets/img/listProduct/product_3.jpeg",   
         name : "Áo Polo Nerdy Brown Sleeves",
         price: "12",
-        type: "áo"
+        type: "áo Polo"
 
     },
     {
@@ -29,7 +29,7 @@
         url : "../assets/img/listProduct/product_4.webp",   
         name : "Áo thun Single Linen Base",
         price: "15",
-        type: "quần"
+        type: "ao thun"
 
     },
     {
@@ -37,28 +37,28 @@
         url : "../assets/img/listProduct/product_5.webp",   
         name : "Quần Dài Orange Label Slim",
         price: "14",
-        type: "quần"
+        type: "Quần dài"
     },
     {
         id : 6,
         url : "../assets/img/listProduct/product_6.webp",   
         name : "Áo Thun Boxy Hugging Beige",
         price: "10",
-        type: "áo"
+        type: "ao thun"
     },
     {
         id : 7,
         url : "../assets/img/listProduct/product_7.webp",   
         name : "Quần Jean Skinny White Label Ironic",
         price: "20",
-        type: "quần"
+        type: "quần Jean"
     },
     {
         id : 8,
         url : "../assets/img/listProduct/product_8.webp",   
         name : "Áo Thun Lolipop White 360GSM",
         price: "13",
-        type: "áo"
+        type: "ao thun"
     },
     {
         id : 9,
--- home/assets/scrip.js
+++ home/assets/scrip.js
@@ -2,16 +2,20 @@ import { listProductData } from "../../data/listProduct.js";
 
 const listProducts =
   JSON.parse(localStorage.getItem("listProducts")) || listProductData;
+
 function createListProduct(listProducts) {
   const container = document.querySelector(".list__product__container");
-  const product = listProducts.map((product) => {
-    return ` <li class="product__item">
+  const product = listProducts.map((product, index) => {
+    return ` <li key=${index} class="product__item" id = ${product.id}>
                 <div class="product__item__wrap">
+                 
                     <img src="${product.url}" alt="" class="product__img">
                     <h2 class="product__name">${product.name}</h2>
                     <span class="product__price">${product.price}$</span>
                 </div>
             </li>`;
+
+            
   });
   container.innerHTML = product.join("");
 }
@@ -27,33 +31,51 @@ itemMenu.forEach((item) => {
   });
 });
 // search top menu
-itemMenu.forEach((item) => {
-  const listProducts = JSON.parse(localStorage.getItem("listProducts"));
-  var listProductResult = [];
-  item.addEventListener("click", () => {
-    const keyWord = chuyenChuoiInHoaKhongDau(item.textContent);
-    if (keyWord == "all") {
-      createListProduct(listProducts);
-      return;
-    } else {
-      let result = false;
+// itemMenu.forEach((item) => {
+//   const listProducts = JSON.parse(localStorage.getItem("listProducts"));
+//   var listProductResult = [];
+//   item.addEventListener("click", () => {
+//     const keyWord = chuyenChuoiInHoaKhongDau(item.textContent);
+//     if (keyWord == "all") {
+//       createListProduct(listProducts);
+//       return;
+//     } else {
+//       let result = false;
 
-      for (let i = 0; i < listProducts.length; i++) {
-        const product = chuyenChuoiInHoaKhongDau(listProducts[i].name);
-        if (product.indexOf(keyWord) !== -1) {
-          listProductResult.push(listProducts[i]);
-          result = true;
-        }
-      }
-      if (result) {
-        createListProduct(listProductResult);
-        listProductResult = [];
-      } else {
-        createListProduct(listProductResult);
-      }
-    }
-  });
-});
+//       for (let i = 0; i < listProducts.length; i++) {
+//         const product = chuyenChuoiInHoaKhongDau(listProducts[i].name);
+//         if (product.indexOf(keyWord) !== -1) {
+//           listProductResult.push(listProducts[i]);
+//           result = true;
+
+//         }
+//         console.log(listProductResult)
+//       }
+//       if (result) {
+//         createListProduct(listProductResult);
+//         listProductResult = [];
+//       } else {
+//         createListProduct(listProductResult);
+//       }
+//     }
+//   });
+// });
+
+itemMenu.forEach((item)=> {
+  item.addEventListener("click",()=> {
+  const listProducts = JSON.parse(localStorage.getItem("listProducts"));
+  console.log(chuyenChuoiInHoaKhongDau(item.textContent))
+  const result = listProducts.filter(product => chuyenChuoiInHoaKhongDau(item.textContent) == chuyenChuoiInHoaKhongDau(product.type))
+  console.log(result)
+  if(result){
+    
+    createListProduct(null)
+    createListProduct(result)
+  }else{
+    createListProduct([])
+  }
+ })
+})
 
 function chuyenChuoiInHoaKhongDau(chuoi) {
   return chuoi
@@ -63,40 +85,42 @@ function chuyenChuoiInHoaKhongDau(chuoi) {
 }
 
 // input tim kiem
-const searchInput = document.getElementById("search__product");
+// const searchInput = document.getElementById("search__product");
 const searchResults = document.querySelector(".btn__search");
 
-searchInput.addEventListener("input", function () {
-  const searchTerm = chuyenChuoiInHoaKhongDau(searchInput.value);
+// searchInput.addEventListener("input", function () {
+//   const searchTerm = chuyenChuoiInHoaKhongDau(searchInput.value);
 
-  const listProducts = JSON.parse(localStorage.getItem("listProducts"));
-  const listProductResult = [];
+//   const listProducts = JSON.parse(localStorage.getItem("listProducts"));
+//   const listProductResult = [];
 
-  if (!searchTerm) {
-    createListProduct(listProducts);
-  } else {
-    let result = false;
+//   if (!searchTerm) {
+//     createListProduct(listProducts);
+//   } else {
+//     let result = false;
 
-    for (let i = 0; i < listProducts.length; i++) {
-      const product = chuyenChuoiInHoaKhongDau(listProducts[i].name);
-      if (product.indexOf(searchTerm) !== -1) {
-        listProductResult.push(listProducts[i]);
-        result = true;
-      }
-    }
-    if (result) {
-      createListProduct(listProductResult);
-    } else {
-      createListProduct(listProductResult);
-    }
-  }
-});
+//     for (let i = 0; i < listProducts.length; i++) {
+//       const product = chuyenChuoiInHoaKhongDau(listProducts[i].name);
+//       if (product.indexOf(searchTerm) !== -1) {
+//         listProductResult.push(listProducts[i]);
+//         result = true;
+//       }
+//     }
+//     if (result) {
+//       createListProduct(null)
+//       createListProduct(listProductResult);
+//     } else {
+//       createListProduct(listProductResult);
+//     }
+//   }
+// });
+
+
+
 
 // search Product
 function searchProduct() {
-  const keyWord = chuyenChuoiInHoaKhongDau(
-    document.getElementById("search__product").value
-  );
+  const keyWord = chuyenChuoiInHoaKhongDau(document.getElementById("search__product").value);
   const listProducts = JSON.parse(localStorage.getItem("listProducts"));
   const listProductResult = [];
 
@@ -115,6 +139,7 @@ function searchProduct() {
     }
 
     if (result) {
+      createListProduct(null)
       createListProduct(listProductResult);
       document.getElementById("search__product").value = "";
     } else {
@@ -125,6 +150,7 @@ function searchProduct() {
 }
 
 searchResults.addEventListener("click", () => {
+
   searchProduct();
 });
 // showProduct
@@ -132,22 +158,32 @@ searchResults.addEventListener("click", () => {
 const productInfo = document.querySelector("#productInfo");
 const callBlocks = document.querySelectorAll(".product__item");
 
-callBlocks.forEach((callBlock, i) => {
-  callBlock.addEventListener("click", () => {
-    productInfo.classList.add("active");
-    productInfo.classList.remove("unactive");
-    document.getElementById("imgbig").src = document.querySelectorAll(".product__img")[i].src;
-    document.getElementById("productname").innerHTML = document.querySelectorAll(".product__name")[i].textContent;
-    document.getElementById("productprice").innerHTML = document.querySelectorAll(".product__price")[i].textContent;
-    const closeBtn = document.querySelector(".close");
-    closeBtn.addEventListener("click", () => {
-      productInfo.classList.remove("active");
-      productInfo.classList.add("unactive");
-      document.querySelector(".quantity-input").value = 1;
-      document.getElementById("size").value = document.getElementById("size").options[0].value;
-    });
+callBlocks.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("onclic")
+    const productID = item.id;
+    const result = listProducts.filter((product) => product.id == productID);
+
+    if (result) {
+      productInfo.classList.add("active");
+      productInfo.classList.remove("unactive");
+
+
+      document.getElementById("imgbig").src = result[0].url;
+      document.getElementById("productname").innerHTML = result[0].name;
+      document.getElementById("productprice").innerHTML =  `${result[0].price}$`;
+      const closeBtn = document.querySelector(".close");
+      
+      closeBtn.addEventListener("click", () => {
+              productInfo.classList.remove("active");
+              productInfo.classList.add("unactive");
+              document.querySelector(".quantity-input").value = 1;
+              document.getElementById("size").value = document.getElementById("size").options[0].value;
+            });
+    }
   });
 });
+
 // Lấy các phần tử HTML cần thao tác
 const decreaseButton = document.querySelector(".decrease");
 const increaseButton = document.querySelector(".increase");
@@ -166,4 +202,3 @@ increaseButton.addEventListener("click", () => {
   let currentValue = parseInt(quantityInput.value);
   quantityInput.value = (currentValue + 1).toString();
 });
-
